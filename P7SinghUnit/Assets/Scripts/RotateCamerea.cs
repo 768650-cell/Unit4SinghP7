443e53321c9c6587e68335128d67a723bd1047bd
768650-cell <768650@student.dusd.net>
2026-04-15 15:20:54 -0700

    Unit4
    I am on SpawnManager.

--- P7SinghUnit/Assets/Scripts/RotateCamerea.cs
+++ P7SinghUnit/Assets/Scripts/RotateCamerea.cs
@@ -1,21 +1,20 @@
 using UnityEngine;
 
-public class RotateCamera : MonoBehaviour
+public class RotateCamerea : MonoBehaviour
 {
 
-    public float rotationSpeed;
-    
+    public float rotateSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up, horizontalInput * rotateSpeed * Time.deltaTime);
     }
 }
---
+++ P7SinghUnit/Assets/Scripts/Spawn Manager.cs
@@ -0,0 +1,30 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+
+       
+        Instantiate(enemyPrefab,new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+
+            
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+        
+    
+}
